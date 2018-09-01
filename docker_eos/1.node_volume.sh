a44793f6852709e5298eddd6b455a8253e2a0f44
newro <newro@gmail.com>
2018-09-01 20:50:12 +0900

    Chg. reduce step

--- docker_eos/1.node_volume.sh
+++ docker_eos/1.node_volume.sh
@@ -1,7 +1,6 @@
 sudo -E docker volume rm local_nodeos_volume
 sudo -E docker volume create local_nodeos_volume
 sudo -E docker run -it --rm -d --name deploy -v local_nodeos_volume:/volume alpine
-sudo -E docker cp local_nodeos_volume_init.tar.bz2 deploy:/.
-sudo -E docker exec -it deploy sh -c "tar -C /volume/ -xjf /local_nodeos_volume_init.tar.bz2"
+sudo -E docker cp config.ini deploy:/volume/.
 sudo -E docker stop deploy
-sudo -E docker network create --attachable -d overlay eos_net
+sudo -E docker network create --attachable eos_net
